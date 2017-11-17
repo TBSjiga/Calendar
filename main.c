c083de064b1fbdcb81396444a53c159b9b9c6f08
TBSjiga <31573154+TBSjiga@users.noreply.github.com>
2017-11-17 19:18:38 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,16 @@ int main()
 	int q , m , k, h;
 	printf ("Please, enter the date. (dd mm yyyy.) \n");
 	scanf ("%d %d %d " , &q , &m , &k);
+	if (m==1)
+	{
+		m=13;
+		k--;
+	}
+	if (m==2)
+	{
+		m=14;
+		k--;
+	}
 	h=((q+(13*(m+1))/5)+(k%100)+((k%100)/4)+(k/400)-2*(k/100))%7;
 	if (h==0)
 	{
